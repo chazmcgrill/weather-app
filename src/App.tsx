f5a448fdf950ead22019a8119eca812d78d79ede
chazmcgrill <charlietaylordev@gmail.com>
2018-10-27 16:18:03 +0100

    Adds Main Components

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,17 @@
 import * as React from 'react';
 import './App.css';
+import Forecast from './components/Forecast';
+import Header from './components/Header';
+import WeatherDisplay from './components/WeatherDisplay';
 
-import logo from './logo.svg';
-
-class App extends React.Component {
+export default class App extends React.Component {
   public render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.tsx</code> and save to reload.
-        </p>
-      </div>
+      <React.Fragment>
+        <Header />
+        <WeatherDisplay />
+        <Forecast />
+      </React.Fragment>
     );
   }
 }
-
-export default App;
