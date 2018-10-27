b1239e3a431ac725593584695798cc1701aa2b0d
chazmcgrill <charlietaylordev@gmail.com>
2018-10-27 17:47:49 +0100

    Adds Api Call

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,37 @@ import Forecast from './components/Forecast';
 import Header from './components/Header';
 import WeatherDisplay from './components/WeatherDisplay';
 
-export default class App extends React.Component {
+interface IState {
+  data: {}
+}
+
+export default class App extends React.Component<{}, IState> {
+  public state: IState = {
+    data: {}
+  }
+
+  public componentDidMount() {
+    this.callApi();
+  }
+
+  public async callApi() {
+    const data = await this.api(process.env.REACT_APP_TEST_API_URL || "");
+    this.setState({ data });
+  }
+  
+  public api<T>(url: string): Promise<T> { 
+    return fetch(url)
+      .then(res => {
+        if (!res.ok) {
+          throw new Error(res.statusText);
+        }
+        return res.json();
+      })
+      .then(data => {
+        return data;
+      });
+  }
+
   public render() {
     return (
       <React.Fragment>
