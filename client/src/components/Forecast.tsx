8b60aa9a9128b0d7f97dee6221053188e14f614a
chazmcgrill <charlietaylorcoder@gmail.com>
2019-02-19 18:23:01 +0000

    Adds Days Of Week to Forecast

--- client/src/components/Forecast.tsx
+++ client/src/components/Forecast.tsx
@@ -14,16 +14,24 @@ export default (props: IForecastProps) => {
     }
 
     const { data: forecast } = props.weather.daily;
+    const days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+
+    let currentDay = new Date().getDay();
 
     return (
         <div className="forecast">
-            {forecast.map((day: any) => (
-                <div key={day.time} className="forecast-card">
-                    <span className={`mdi mdi-${WeatherIcons.weatherType(day.icon).iconSlug}`} />
-                    <span>{NumberFormat.from(day.temperatureMax).degreesC}</span>
-                    <span>{NumberFormat.from(day.temperatureMin).degreesC}</span>
-                </div>
-            ))}
+            {forecast.map((day: any, i: number) => {
+                const index = currentDay + i > 6 ? currentDay + i - 7 : currentDay + i;
+                const dayText = days[index];
+                return (
+                    <div key={day.time} className="forecast-card">
+                        <span>{dayText}</span>
+                        <span className={`mdi mdi-${WeatherIcons.weatherType(day.icon).iconSlug}`} />
+                        <span>{NumberFormat.from(day.temperatureMax).degreesC}</span>
+                        <span>{NumberFormat.from(day.temperatureMin).degreesC}</span>
+                    </div>
+                )
+            })}
         </div>
     );
 }
