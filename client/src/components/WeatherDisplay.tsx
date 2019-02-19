c8372d9492ca20c464db7546d55aaf5e16f7e4a9
chazmcgrill <charlietaylorcoder@gmail.com>
2019-02-19 18:23:25 +0000

    Adds Additional Stats to Current Weather

--- client/src/components/WeatherDisplay.tsx
+++ client/src/components/WeatherDisplay.tsx
@@ -15,11 +15,24 @@ export default (props: IWeatherDisplayProps) => {
     
     return (
         <div className="weather-display">
-            <h1>Location</h1>
+            <h1>New York</h1>
             <h2>{NumberFormat.from(props.weather.currently.temperature).degreesC}</h2>
-            <span className={`mdi mdi-${WeatherIcons.weatherType(props.weather.currently.icon).iconSlug}`} />
+            <span className={`weather-display--icon mdi mdi-${WeatherIcons.weatherType(props.weather.currently.icon).iconSlug}`} />
             <h3>{props.weather.currently.summary}</h3>
             <p>{props.weather.hourly.summary}</p>
+
+            <div>
+                <span className="mdi mdi-weather-windy" />
+                <span>{`${props.weather.currently.windSpeed}mph`}</span>
+            </div>
+            <div>
+                <span className="mdi mdi-umbrella" />
+                <span>{`${props.weather.currently.humidity * 100}%`}</span>
+            </div>
+            <div>
+                <span>UV Index: </span>
+                <span>{props.weather.currently.uvIndex}</span>
+            </div>
         </div>
     );
 };
